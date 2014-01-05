06d25a8f8eac839906ef9887431f7d630a9cae59
Orvid King <blah38621@gmail.com>
2014-01-05 13:22:37 -0600

    Moved the elevator source to the new source structure, and formatted it to match the rest of NeoAxis.

--- Project/Src/ProjectEntities/Community/Elevator.cs
+++ Project/Src/ProjectEntities/Community/Elevator.cs
@@ -3,7 +3,7 @@ using Engine;
 using Engine.MathEx;
 using Engine.PhysicsSystem;
 
-namespace ProjectEntities
+namespace ProjectEntities.Community
 {
 	public class ElevatorType : DynamicType
 	{
@@ -26,58 +26,58 @@ namespace ProjectEntities
 
 		ElevatorType _type = null; public new ElevatorType Type { get { return _type; } }
 
-		[Description("The highest point above the start position that the elevator will reach.")]
-		[DefaultValue(10f)]
+		[Description( "The highest point above the start position that the elevator will reach." )]
+		[DefaultValue( 10f )]
 		public float TopZ
 		{
 			get { return maxElevation; }
 			set { maxElevation = value; }
 		}
 
-		[Description("The speed at which to move the elevator.")]
-		[DefaultValue(1f)]
+		[Description( "The speed at which to move the elevator." )]
+		[DefaultValue( 1f )]
 		public float Velocity
 		{
 			get { return velocity; }
 			set { velocity = value; }
 		}
 
-		[Description("Determines if the elevator is in motion or not.")]
-		[DefaultValue(false)]
+		[Description( "Determines if the elevator is in motion or not." )]
+		[DefaultValue( false )]
 		public bool PlatformMoving
 		{
 			get { return platformMoving; }
 			set { platformMoving = value; }
 		}
 
-		[Description("Determines if the elevator should go back to the bottom, and repeat the process after reaching the top.")]
-		[DefaultValue(false)]
+		[Description( "Determines if the elevator should go back to the bottom, and repeat the process after reaching the top." )]
+		[DefaultValue( false )]
 		public bool Cycle
 		{
 			get { return cycle; }
 			set { cycle = value; }
 		}
 
-		protected override void OnPostCreate(bool loaded)
+		protected override void OnPostCreate( bool loaded )
 		{
-			base.OnPostCreate(loaded);
-			initialZ = PhysicsModel.Bodies[0].Position.Z;
+			base.OnPostCreate( loaded );
+			initialZ = PhysicsModel.Bodies[ 0 ].Position.Z;
 			SubscribeToTickEvent();
 		}
 
 		protected override void OnTick()
 		{
 			base.OnTick();
-			if (PlatformMoving == true)
+			if( PlatformMoving == true )
 			{
-				PhysicsModel.Bodies[0].LinearVelocity = new Vec3(0, 0, velocity * direction);
+				PhysicsModel.Bodies[ 0 ].LinearVelocity = new Vec3( 0, 0, velocity * direction );
 
-				if ((PhysicsModel.Bodies[0].Position.Z >= (initialZ + maxElevation) && direction == 1) ||
-					(PhysicsModel.Bodies[0].Position.Z <= initialZ && direction == -1))
+				if( ( PhysicsModel.Bodies[ 0 ].Position.Z >= ( initialZ + maxElevation ) && direction == 1 ) ||
+					( PhysicsModel.Bodies[ 0 ].Position.Z <= initialZ && direction == -1 ) )
 				{
-					if (cycle == false)
+					if( cycle == false )
 						PlatformMoving = false;
-					PhysicsModel.Bodies[0].LinearVelocity = Vec3.Zero;
+					PhysicsModel.Bodies[ 0 ].LinearVelocity = Vec3.Zero;
 					direction = -direction;
 				}
 			}
